9f5514ab1ab336b77602c24833f0c32c57e3a8c8
Riley Lannon <lannon.riley@gmail.com>
2023-02-26 17:20:28 -0500

    Begin promise work

---
+++ include/promise.hxx
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "rtl.hxx"
+#include "message_receiver.hxx"
+
+URA_RTL_BEGIN
+
+class Promise: public MessageReceiver
+{
+    // todo:
+};
+
+URA_RTL_END
--- src/await.cxx
+++ src/await.cxx
@@ -1,7 +1,29 @@
 #include "await.hxx"
+#include "promise.hxx"
+#include "message.hxx"
+#include "actor.hxx"
 
 URA_RTL_BEGIN
 
-// todo: implement promise/await...
+void await(Promise& p, Message& m, Actor& recipient)
+{
+    // todo:
+}
+
+void await(Promise& p)
+{
+    // todo:
+}
+
+void await_forward(Promise& p, Message& m, Actor& recipient)
+{
+    // todo:
+}
+
+void await_meanwhile(Promise& p, Message& m, Actor& recipient)
+{
+    // todo:
+}
+
 
 URA_RTL_END
